37aeaacfc625f81b1e5fc05a0d5d2f681c0d97f0
Esdran26 <vargasandres2627@gmail.com>
2020-02-06 09:20:07 -0500

    Add Insert Users in Register.php

--- Back-End/Register/register.php
+++ Back-End/Register/register.php
@@ -14,8 +14,14 @@ if($_POST) {
 
     $passwordHash = password_hash($password, PASSWORD_DEFAULT);
 
+    /*  */
     if(password_verify($usVerifyPassword, $passwordHash)) {
         include_once '../Config/connection.php';
+        $sqlInsertUser = 'INSERT INTO users (us_name,us_lastname,us_email,us_password) VALUES(?,?,?,?)';
+        $sentenceInsertUser = $pdo->prepare($sqlInsertUser);
+        $sentenceInsertUser->execute([$name,$lastName,$email,$passwordHash]);
+        
+        header('location:../../Front-End/Login/login.html');
     }
     else {
         die();
