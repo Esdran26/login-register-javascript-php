efd6f45179408cf01ee0f2fb15c245fc15314927
Esdran26 <vargasandres2627@gmail.com>
2020-02-13 18:30:41 -0500

    Create Products Table in scriptDB

--- Back-End/SQL/scriptDB.sql
+++ Back-End/SQL/scriptDB.sql
@@ -1,7 +1,13 @@
 CREATE TABLE users (
-    us_id integer AUTO_INCREMENT PRIMARY KEY,
+    us_id INTEGER AUTO_INCREMENT PRIMARY KEY,
     us_name VARCHAR(255) NOT NULL,
     us_lastname VARCHAR(255) NOT NULL,
     us_email VARCHAR(255) UNIQUE NOT NULL,
     us_password VARCHAR(255) NOT NULL
+);
+CREATE TABLE products (
+    prod_id BIGINT PRIMARY KEY,
+    prod_nombre VARCHAR(255) NOT NULL,
+    prod_precio INTEGER NOT NULL,
+    prod_cantidad INTEGER NOT NULL
 );
